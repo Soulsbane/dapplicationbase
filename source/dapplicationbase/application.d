10819f9f96b6d669c8a158f79ce659be121ca65e
Paul Crane <paul@acheronsoft.com>
2017-04-28 23:18:53 -0500

    Add initial methods for loading and handling options.

--- source/dapplicationbase/application.d
+++ source/dapplicationbase/application.d
@@ -1,6 +1,5 @@
 module dapplicationbase.application;
 
-
 private interface EmptyBase {}
 private struct OptionsBase {}
 
@@ -12,12 +11,30 @@ mixin template ApplicationMixin(AppOptions = OptionsBase, InheritedClass = Empty
 
 	class Application : InheritedClass
 	{
+	public:
 		this(const string organizationName, const string applicationName) @safe
 		{
 			path_.create(organizationName, applicationName);
 		}
 
 		void create(string[] arguments)
+		{
+			createDirs("config");
+		}
+
+		void createDirs(T...)(T dirs)
+		{
+			foreach(dir; dirs)
+			{
+				path_.createDir(dir);
+			}
+		}
+
+		void loadOptions()
+		{
+		}
+
+		void handleCmdLineArguments(string[] arguments)
 		{
 			try
 			{
