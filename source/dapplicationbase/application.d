d4eaa4fcb5a72a8599f15b631d0ffd5638d7746e
Paul Crane <paul@acheronsoft.com>
2017-05-10 02:44:48 -0500

    Work around missing multiple inheritance support in D.

--- source/dapplicationbase/application.d
+++ source/dapplicationbase/application.d
@@ -6,6 +6,7 @@ public import std.stdio;
 public import dpathutils.config;
 public import ctoptions.getoptmixin;
 public import ctoptions.structoptions;
+public import std.getopt;
 
 private interface EmptyBase {}
 private struct OptionsBase {}
@@ -15,6 +16,35 @@ mixin template ApplicationMixin(AppOptions = OptionsBase, InheritedClass = Empty
 	class Application : InheritedClass
 	{
 	public:
+		this()
+		{
+			// We has to jump through a bunch of hoops here since D doesn't support multiple inheritance.
+			this.getOptGen_ = new GetOptCodeGenerator!(AppOptions);
+			this.getOptGen_.setCallback!"onNoArguments"(&onNoArguments);
+		}
+
+		// We have to reimplement these callbacks since alias this doesn't support override of a member function
+		// in the inherited class.
+		void onNoArguments() { writeln("GetOptCodeGenerator.onNoArguments"); }
+
+		void onHelp(GetoptResult helpInformation, CustomHelpFunction func = &defaultGetoptPrinter)
+		{
+			func("The following options are available:", helpInformation.options);
+		}
+
+		void onValidArgument() {}
+
+		void onUnknownArgument(const string msg)
+		{
+			writeln(msg, ". For a list of available commands use --help.");
+		}
+
+		void onInvalidArgument(const string msg)
+		{
+			writeln("Invalid Argument!");
+			writeln(msg);
+		}
+
 		void create(const string organizationName, const string applicationName)
 		{
 			path_.create(organizationName, applicationName);
@@ -40,18 +70,13 @@ mixin template ApplicationMixin(AppOptions = OptionsBase, InheritedClass = Empty
 
 		void handleCmdLineArguments(string[] arguments)
 		{
-			getOptGen_ = new GetOptCodeGenerator!(AppOptions);
 			getOptGen_.generate(arguments, options_);
-		}
-
-		void setCallback(alias name, Func)(Func func)
-		{
-			getOptGen_.setCallback!(name)(func);
 		}
 
 	protected:
 		ConfigPath path_;
 		StructOptions!AppOptions options_;
 		GetOptCodeGenerator!(AppOptions) getOptGen_;
+		alias getOptGen_ this;
 	}
 }
