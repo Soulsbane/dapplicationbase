4d57aeb7b92613d569d0a185e3d97a62619af1c9
Paul Crane <paul@acheronsoft.com>
2019-06-14 02:42:15 -0500

    Add forwarding of StructOptions and ConfigPath variables to opDispatch.

--- source/dapplicationbase/settings.d
+++ source/dapplicationbase/settings.d
@@ -1,1 +1,26 @@
 module dapplicationbase.settings;
+
+public import ctoptions.structoptions;
+public import dpathutils.config;
+
+struct Settings(T)
+{
+	auto opDispatch(string name, Args...)(Args args)
+	{
+		static if(__traits(hasMember, path_, name))
+		{
+			return __traits(getMember, path_, name)(args);
+		}
+
+		static if(__traits(hasMember, options_, name))
+		{
+			return __traits(getMember, options_, name)(args);
+		}
+	}
+
+	alias Options = options_;
+	alias Path = path_;
+
+	StructOptions!T options_;
+	ConfigPath path_;
+}
